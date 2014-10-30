50f424b7c1445f62f35d81e237dfeb3b924d9323
nebiysinna@gmail.com <nebiysinna@gmail.com>
2014-10-29 23:21:59 -0400

    Add MBTE

--- MuchMoreMoods/src/com/teamkhan/muchmoremoods/MainActivity.java
+++ MuchMoreMoods/src/com/teamkhan/muchmoremoods/MainActivity.java
@@ -1,36 +1,67 @@
 package com.teamkhan.muchmoremoods;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {
+	
+	Button btnDescrPage, btnTransfer;
+	public Spinner spinnerMood, spinnerBehaviour, spinnerEnvironment, spinnerThought;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		btnDescrPage = (Button) findViewById(R.id.btnDscrPage);
+		btnTransfer = (Button) findViewById(R.id.btnTransfer);
+		spinnerMood = (Spinner)findViewById(R.id.spinnerMood);
+		spinnerBehaviour = (Spinner)findViewById(R.id.spinnerBehaviour);
+		spinnerEnvironment = (Spinner)findViewById(R.id.spinnerEnvironment);
+		spinnerThought= (Spinner) findViewById(R.id.spinnerThought);
+		
+		btnDescrPage.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent intent = new Intent(MainActivity.this,
+						MBTEDescription.class);
+				startActivity(intent);
+				
+			}
+		});
+		
+		btnTransfer.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String mood = spinnerMood.getSelectedItem().toString();
+				String behav = spinnerBehaviour.getSelectedItem().toString();
+				String envi = spinnerEnvironment.getSelectedItem().toString();
+				String thought = spinnerThought.getSelectedItem().toString();
+				
+				
+					Intent im = new Intent(MainActivity.this ,MBTEDescription.class);
+					im.putExtra("Data1", mood);
+					im.putExtra("Data2", behav);
+					im.putExtra("Data3", envi);
+					im.putExtra("Data4", thought);
+					startActivity(im);
+				
+				
+				}
+		});
+	}
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	
 }
